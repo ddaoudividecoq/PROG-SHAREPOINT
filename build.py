619fc15470ebde870936b22c8ce426b699970457
ddaoudividecoq <ddaoudividecoq@groupe-pec.com>
2026-09-14 11:46:23 +0200

    Update ACCOR_LINK to point to the programming page

--- build.py
+++ build.py
@@ -23,7 +23,7 @@ BATACLAN_URL = "https://billetterie.bataclan.fr/fr/hubber/catalog/resource/ticke
 
 # --- Lien "au clic" pour les salles Groupe PEC (leur JSON n'a pas de lien par evenement) ---
 # >>> A CONFIRMER : remplace par la vraie page programmation de chaque salle.
-ACCOR_LINK  = "https://www.accorarena.com"
+ACCOR_LINK  = "https://www.accorarena.com/fr/programmation"
 ADIDAS_LINK = "https://www.adidasarena.com/programmation"
 
 OUT = "docs/events.json"
